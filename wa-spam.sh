1feb2d30dd6036b686727afc178034d8d0a0d325
rafiramzi <79634000+rafiramzi@users.noreply.github.com>
2022-12-09 11:15:54 +0700

    Add files via upload

---
+++ wa-spam.sh
@@ -0,0 +1,22 @@
+clear
+echo - Connect your phone to this laptop using USB Cable
+echo - Android Only
+echo 
+echo
+
+read -p 'Message: ' uservar
+read -p 'Country Code: ' usercod
+read -p 'phone number: ' userph
+adb shell am start -a android.intent.action.VIEW -d "https://api.whatsapp.com/send?phone=+[$usercod][$userph]&msg=$uservar"
+sleep 5 
+for (( ; ; ))
+do
+   echo "Press (ctrl + c) to stop"
+   adb shell input text "$uservar"
+   adb shell input keyevent 66
+   clear
+done
+
+
+
+
